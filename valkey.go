85e4d9fa1a1130d082ff3a25af49d09733dee9ba
HenTaku <hentaku@duck.com>
2025-05-30 12:25:54 +0800

    Update valkey.go

--- valkey.go
+++ valkey.go
@@ -12,13 +12,13 @@ type Client struct {
 }
 
 // NewClient 若启用 tls 但无更多配置需传入 &tls.Config{} 而不是 nil
-func NewClient(address, password string, db int, forceSingleClient bool, tlsConfig *tls.Config) (*valkey.Client, error) {
+func NewClient(address, password string, db int, forceSingleClient bool, tlsConfig *tls.Config) (Client, error) {
 	client, err := valkey.NewClient(valkey.ClientOption{InitAddress: []string{address}, Password: password, SelectDB: db, ForceSingleClient: forceSingleClient, TLSConfig: tlsConfig})
 	if err != nil {
 		return nil, err
 	}
 
-	return &client, nil
+	return &Client{client, context.Background()}, nil
 }
 
 func (c *Client) Delete(key string) error {
